464d09d144d8deef9c0d2e9a080d48ccab187a0b
nehriienko <sasha.negrienko2007@gmail.com>
2025-12-09 23:12:52 +0200

    10. LinkedList tasks - done

--- LinkedList/LinkedList/src/Main.java
+++ LinkedList/LinkedList/src/Main.java
@@ -1,38 +1,26 @@
-import java.util.Collections;
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list1 = new LinkedList<Integer>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(4);
-        LinkedList<Integer> list2 = new LinkedList<Integer>();
-        list2.add(1);
-        list2.add(2);
-        list2.add(4);
-        list1.addAll(list2);
-        Collections.sort(list1);
-        for (Integer  i : list1 ) {
-            System.out.println(i);
-        }
 
-        list1.clear();
-        list2.clear();
-        list1.addAll(list2);
-        Collections.sort(list1);
-        for (Integer i : list1) {
-            System.out.println(i);
-        }
+        // перший список
+        ListNode list1 = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(4)));
 
-        System.out.println();
+        // другий список
+        ListNode list2 = new ListNode(1,
+                new ListNode(3,
+                        new ListNode(4)));
 
-        list1.clear();
-        list2.add(0);
-        list1.addAll(list2);
-        Collections.sort(list1);
-        for (Integer i : list1) {
-            System.out.println(i);
+        Solution s = new Solution();
+        ListNode merged = s.mergeTwoLists(list1, list2);
+
+        printList(merged);
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
         }
     }
 }
